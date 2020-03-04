b220c723ab631e418d515ed750fab9cb834ce064
Igor Rafael <igor.rafael@gmail.com>
2020-03-04 13:23:51 -0300

    refactor(forms): unify form group components into a single file

--- src/forms/FormGroup.jsx
+++ src/forms/FormGroup.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import { FormLabel } from './FormLabel';
+import { FormInput } from './FormInput';
+import { FormSelect } from './FormSelect';
+import { FormSwitch } from './FormSwitch';
 
-export function FormGroup({ children, ...props }) {
+function FormGroup({ children, ...props }) {
   return (
     <div className="form-group">
       <FormLabel {...props} />
       {children}
     </div>
+  );
+}
+
+export function FormGroupInput(props) {
+  return (
+    <FormGroup {...props}>
+      <FormInput {...props} />
+    </FormGroup>
+  );
+}
+
+export function FormGroupSelect(props) {
+  return (
+    <FormGroup {...props}>
+      <FormSelect {...props} />
+    </FormGroup>
+  );
+}
+
+export function FormGroupSwitch(props) {
+  return (
+    <FormGroup {...props}>
+      <FormSwitch {...props} />
+    </FormGroup>
   );
 }
