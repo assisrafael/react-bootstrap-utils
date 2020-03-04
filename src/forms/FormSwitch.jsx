e64247f1ddfed9c6c325c92fe22971c33899a0c1
Igor Rafael <igor.rafael@gmail.com>
2020-03-04 13:23:06 -0300

    feat(forms): implements FormSwitch component

--- src/forms/FormSwitch.jsx
+++ src/forms/FormSwitch.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { FormContext, handleInputChange } from './form-helpers';
 
-export function FormSwitch() {
+export function FormSwitch({ id, name, required, trueLabel, falseLabel }) {
+  const formState = useContext(FormContext);
+
+  function onChange(e) {
+    const { name, value } = handleInputChange(e);
+
+    formState.update(name, value);
+  }
+
+  const value = formState.getValue(name) || false;
+
   return (
-    <div class="custom-control custom-switch">
-      <input type="checkbox" class="custom-control-input" id="customSwitch1" />
-      <label class="custom-control-label" for="customSwitch1">
-        Toggle this switch element
+    <div className="custom-control custom-switch">
+      <input
+        {...{ required, name, id }}
+        type="checkbox"
+        className="custom-control-input"
+        onChange={onChange}
+        checked={value}
+      />
+      <label className="custom-control-label" htmlFor={id}>
+        {(trueLabel || falseLabel) && (value ? trueLabel : falseLabel)}
       </label>
     </div>
   );
 }
+
+FormSwitch.defaultProps = {
+  showValueLabel: false,
+};
+
+FormSwitch.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  trueLabel: PropTypes.string,
+  falseLabel: PropTypes.string,
+};
