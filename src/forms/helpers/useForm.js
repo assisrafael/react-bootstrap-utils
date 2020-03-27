4be36e2c9016524604741ae6ba8ce3e95e21eba1
Igor Rafael <igor.rafael@gmail.com>
2020-03-27 09:17:29 -0300

    fix(forms): reset submitAttempted state on form reset

--- src/forms/helpers/useForm.js
+++ src/forms/helpers/useForm.js
@@ -45,6 +45,7 @@ export function useForm(initialState, validations) {
     },
     reset() {
       setFormState(initialState);
+      setSubmitAttempted(false);
     },
     setSubmitedAttempted() {
       setSubmitAttempted(true);
