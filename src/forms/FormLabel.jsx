40957434fb3a91db6457d31bf59b963f877921cb
Igor Rafael <igor.rafael@gmail.com>
2020-05-27 14:24:46 -0300

    fix(forms): show visual indication of required fields

--- src/forms/FormLabel.jsx
+++ src/forms/FormLabel.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export function FormLabel({ id, label }) {
-  return <label htmlFor={id}>{label}</label>;
+export function FormLabel({ id, label, required }) {
+  return (
+    <label htmlFor={id}>
+      {label}
+
+      {required && <span className="text-danger"> *</span>}
+    </label>
+  );
 }
 
 FormLabel.propTypes = {
   id: PropTypes.string,
   label: PropTypes.string,
+  required: PropTypes.bool,
 };
