ef30a71f8676e318ef33db41590c1ef826fe49fb
Igor Rafael <igor.rafael@gmail.com>
2020-08-13 11:42:51 -0300

    fix(toasts): improve code completion with jsdocs

--- src/toasts/useToasts.js
+++ src/toasts/useToasts.js
@@ -5,7 +5,18 @@ export function useToasts() {
   const toastsState = useContext(ToastsContext);
 
   return {
-    showToast(message, { type = 'info', autoClose = 5000, position = 'TOP_RIGHT' } = {}) {
+    /**
+     * showToast
+     * @param {string} message
+     * @param {object} options Configurations object
+     * @param {string} options.type toast color based on bootstrap theme ('info' - default, 'success', 'danger', 'warning')
+     * @param {number} options.autoClose miliseconds to automatically close toast (default 5000)
+     * @param {string} options.position toast position ('TOP_LEFT', 'TOP_RIGHT' - default, 'BOTTOM_LEFT', 'BOTTOM_RIGHT')
+     *
+     * @returns {func} a closeToast function to close the opened toast
+     */
+    showToast(message, options) {
+      const { type = 'info', autoClose = 5000, position = 'TOP_RIGHT' } = options || {};
       const toastId = toastsState.show(message, { type, autoClose, position });
 
       return function closeToast() {
