b8070490f6639d094b1dbee91e7e819326b72631
Igor Rafael <igor.rafael@gmail.com>
2020-12-03 11:41:11 -0300

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,22 @@
 /* eslint-disable import/no-anonymous-default-export */
 /* eslint-disable import/no-default-export */
 import resolve from '@rollup/plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/main.js',
-    format: 'es',
-  },
   plugins: [
     resolve({
       extensions: ['.mjs', '.js', '.json', '.jsx'],
     }),
     babel({
-      exclude: 'node_modules/**', // only transpile our source code
+      babelHelpers: 'runtime',
+      exclude: /node_modules/, // only transpile our source code
     }),
   ],
-  external: ['react', 'prop-types', 'react-modal', 'react-dom', 'js-var-types'],
+  output: {
+    file: 'dist/main.js',
+    format: 'es',
+  },
+  external: [/@babel\/runtime/, 'react', 'prop-types', 'react-modal', 'react-dom', 'js-var-type'],
 };
